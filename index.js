bdebd8c3d35131df0209b1be93e8edf715dadd42
Deepkamal Singh <deesingh@adobe.com>
2021-08-20 07:45:42 +0530

    Added configs

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-var smpp = require('smpp');
-var session = smpp.connect({
-    url: 'smpp://example.com:2775',
-    auto_enquire_link_period: 10000
-});
-session.bind_transceiver({
-    system_id: 'YOUR_SYSTEM_ID',
-    password: 'YOUR_PASSWORD'
-}, function(pdu) {
+let session_config = {
+    connect:{
+        url: 'smpp://197.210.3.229 :10000',
+        auto_enquire_link_period: 10000
+    },
+    bind:{
+        system_id: 'GTEL496408Bulk',
+        password: '16ah!^AH'
+    }
+}
+
+let smpp = require('smpp');
+let session = smpp.connect(session_config.connect);
+session.bind_transmitter(session_config.bind, function(pdu) {
     if (pdu.command_status == 0) {
         // Successfully bound
         session.submit_sm({
-            destination_addr: 'DESTINATION NUMBER',
-            short_message: 'Hello!'
+            destination_addr: '+2348033897513',
+            short_message: 'Hello from DKS!'
         }, function(pdu) {
             if (pdu.command_status == 0) {
                 // Message successfully sent
